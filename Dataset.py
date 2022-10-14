4e071322a3c3ec32181dd6b12c22d87d6956b6b3
JoshuaMukherjee <joshuaakmukherjee@gmail.com>
2022-10-14 13:37:45 +0100

    dataset

--- Dataset.py
+++ Dataset.py
@@ -57,9 +57,9 @@ class TimeDataset(Dataset):
 
 
 if __name__ == "__main__":
-    timestamps = 2
-    length = 10
-    test_length = 5
+    timestamps = 1
+    length = 30000
+    test_length = 5000
     N = 4
     train = TimeDataset(length,timestamps,N=N)
     torch.save(train,"Datasets/Train-"+str(timestamps)+"-"+str(length)+"-"+str(N)+".pth")
