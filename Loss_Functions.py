2f634d53b1b4c8e324768d3e4f8ecffa39d65983
JoshuaMukherjee <joshuaakmukherjee@gmail.com>
2022-10-21 15:18:23 +0100

    mse

--- Loss_Functions.py
+++ Loss_Functions.py
@@ -3,3 +3,6 @@ import torch
 def max_loss(pressure, true):
   m = torch.max((true-pressure)**2,1).values
   return torch.sum(m,0)
+
+def mse_loss(expected, found):
+  return torch.nn.MSELoss()(expected,found)
