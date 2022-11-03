1dbe7c13f36e04e710230fbcd8a619e11021af52
JoshuaMukherjee <joshuaakmukherjee@gmail.com>
2022-11-03 11:50:46 +0000

    std

--- Autoencoder/Autoencoder_funcs.py
+++ Autoencoder/Autoencoder_funcs.py
@@ -26,8 +26,8 @@ def scale_stack_to_vector(x):
     real = AE_var(x)
     vect = torch.reshape(real,(x.shape[0],-1))
     vect -= vect.mean(0, keepdim=True)
-    sd =vect.std(0, unbiased=False, keepdim=True)
-    if sd != 0:
+    sd = vect.std(0, unbiased=False, keepdim=True)
+    if torch.all(sd != 0):
         vect /= sd
     return vect
 
