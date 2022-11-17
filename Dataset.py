2d2720723bec8f65e91b8bdb7df54d47d29ac3f9
JoshuaMukherjee <joshuaakmukherjee@gmail.com>
2022-11-17 13:39:29 +0000

    Atomic Dataset

--- Dataset.py
+++ Dataset.py
@@ -60,26 +60,100 @@ class TimeDataset(Dataset):
          return self.points[i], self.changes[i],self.activations[i],self.pressures[i]
 
 
+
+class TimeDatasetAtomic(Dataset):
+    '''
+    outputs points, changes, activations, pressures
+    '''
+    def __init__(self,length,timestamps,N=4,threshold = 0.01,seed=None,sort_fun=None,movement=0.01):
+        self.length = length #Number of point sets in the Dataset (length)
+        self.timestamps = timestamps #number of changes in an element
+        self.N = N #Number of points per set
+        self.seed = seed #custom seed
+        self.sort_fun = sort_fun #allows sorting the points within a set
+        self.threshold = threshold #The ammount a point can change by +/-
+        self.movement = movement #The amount to move a point per timestep
+    
+        self.points = []
+        self.changes = []
+        self.activations = []
+        self.pressures = []
+        print(self.length)
+        for batch in range(self.length):
+            p = torch.FloatTensor(3,self.N).uniform_(-.06,.06).to(device)
+            changes = []
+            for t in range(timestamps):
+                change = torch.zeros(3,self.N)
+                index_x = torch.randint(low=0,high=change.shape[0],size=(1,)).item()
+                index_y = torch.randint(low=0,high=change.shape[1],size=(1,)).item()
+                change[index_x,index_y] = self.movement
+                changes.append(change)
+            changes = torch.stack(changes)
+            changes[0,:,:] = 0
+            changes[0,:,:] = 0
+            time_series = torch.zeros_like(changes)
+            change = 0
+            for j,dxyz in enumerate(changes):
+                change += dxyz
+                time_series[j] = p + change
+            
+            # self.points.append(time_series.permute(0,2,1)) 
+            # self.changes.append(changes.permute(0,2,1))
+
+            self.points.append(time_series) 
+            self.changes.append(changes)
+            
+
+            pressures = torch.zeros((self.timestamps,self.N)) + 0j
+            activations = torch.zeros(self.timestamps,512) + 0j
+            
+            for i,points in enumerate(time_series):
+                A=forward_model(points, transducers()).to(device)
+                _, _, x = wgs(A,torch.ones(self.N,1).to(device)+0j,200)
+                pressures[i] = A@x[:,0]
+                activations[i] = x[:,0]
+            self.pressures.append(pressures)
+            self.activations.append(activations)
+
+            if batch % 200 == 0:
+                print(batch,end=" ",flush=True)
+    
+    def __len__(self):
+        return self.length
+
+    def __getitem__(self,i):
+         return self.points[i], self.changes[i],self.activations[i],self.pressures[i]
+
+
 if __name__ == "__main__":
     timestamps = 2
     length = 120000
     test_length = 5000
     N = 4
-    train = TimeDataset(length,timestamps,N=N)
-    torch.save(train,"Datasets/Train-"+str(timestamps)+"-"+str(length)+"-"+str(N)+".pth")
+    train = TimeDatasetAtomic(length,timestamps,N=N)
+    torch.save(train,"Datasets/Train-"+str(timestamps)+"-"+str(length)+"-"+str(N)+"A.pth")
 
-    if test_length > 0:
-        test = TimeDataset(test_length,timestamps,N=N)
-        torch.save(test,"Datasets/Test-"+str(timestamps)+"-"+str(test_length)+"-"+str(N)+".pth")
+    # if test_length > 0:
+    #     test = TimeDataset(test_length,timestamps,N=N)
+    #     torch.save(test,"Datasets/Test-"+str(timestamps)+"-"+str(test_length)+"-"+str(N)+".pth")
     
    
-    # dataset = TimeDataset(10,2)
+
+
+    # dataset = TimeDatasetAtomic(10,2)
     # from torch.utils.data import DataLoader
     # data = DataLoader(dataset)
-    # p,c,a,pr = next(iter(data))
-    # print(a[:,0,:].shape)
-    # # i = 1
-    # # print(torch.abs(propagate(a[:,i,:],p[:,i,:])))
-    # # print(p)
-    # # print(torch.abs(pr))
+    # d = iter(data)
+    # p,c,a,pr = next(d)
+    # print(p)
+    # print(c)
+    # p,c,a,pr = next(d)
+    # print(p)
+    # print(c)
+    
+    
+    # i = 1
+    # print(torch.abs(propagate(a[:,i,:],p[:,i,:])))
+    # print(p)
+    # print(torch.abs(pr))
 
--- Results/Results.py
+++ Results/Results.py
@@ -1,10 +1,10 @@
 import os,sys
-import torch
+import torch,pickle
 from torch.utils.data import DataLoader 
 
 import matplotlib.pyplot as plt
 import matplotlib
-
+matplotlib.use('tkagg')
 
 p = os.path.abspath('.')
 sys.path.insert(1, p)
@@ -15,28 +15,54 @@ from Networks import PointNet, MLP
 from Dataset import TimeDataset
 
 args = sys.argv
+BOXPLOTS = False
+LOSS = False
+
 try:
     path = args[1]
     net = torch.load("SavedModels/"+path+"/"+"model_"+path+".pth",map_location=device)
+    BOXPLOTS = "-p" in args
+    LOSS = "-l" in args
+
+
 except IndexError:
     print("Invalid Arguments")
 
-dataset = TimeDataset(10,2)
-data = iter(DataLoader(dataset,1,shuffle=True))
+if BOXPLOTS:
+    dataset = TimeDataset(10,2)
+    data = iter(DataLoader(dataset,1,shuffle=True))
 
-N = 5
+    N = 5
 
-pressures = []
+    pressures = []
 
-for i in range(N):
-    p,c,a,pr = next(data)
-    activation_init = a[:,0,:]
-    net.init(activation_init)
-    change = c[:,1,:,:] #Get batch
-    activation_out = net(change)
-    pressure_out = torch.abs(propagate(activation_out,p[:,1,:]))
-    pressures.append(pressure_out)
+    for i in range(N):
+        p,c,a,pr = next(data)
+        activation_init = a[:,0,:]
+        net.init(activation_init)
+        change = c[:,1,:,:] #Get batch
+        activation_out = net(change)
+        pressure_out = torch.abs(propagate(activation_out,p[:,1,:]))
+        pressures.append(pressure_out)
 
-pressure = [p.detach().numpy() for p in pressures]
-print(pressures)
-# plt.boxplot(pressures)
+
+    pressures = [p.detach().numpy() for p in pressures]
+
+    print(pressures)
+    plt.boxplot(pressures)
+    plt.title("Pressures at points")
+    plt.xlabel("Point")
+    plt.ylabel("P")
+    plt.show()
+
+if LOSS:
+    loss = pickle.load(open("SavedModels/"+path+"/"+"loss_"+path+".pth","rb"))
+    train,test = loss
+    print(test,train)
+    plt.plot(train,label="train")
+    plt.plot(test,label="test")
+    plt.yscale("log")
+    plt.legend()
+    plt.xlabel("epoch")
+    plt.ylabel("loss")
+    plt.show()
