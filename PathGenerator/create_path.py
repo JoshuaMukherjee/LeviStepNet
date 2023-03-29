96ed8523837ff4403a2cca4911185a23ae706afc
JoshuaMukherjee <joshuaakmukherjee@gmail.com>
2023-03-29 13:58:09 +0100

    Temporal

--- PathGenerator/create_path.py
+++ PathGenerator/create_path.py
@@ -5,7 +5,7 @@ import torch
 import json
 
 from Utlilities import *
-from Solvers import wgs
+from Solvers import wgs, temporal_wgs
 
 import matplotlib.pyplot as plt
 import matplotlib
@@ -17,7 +17,12 @@ file = sys.argv[1]
 path = sys.argv[2]
 shuffle = "-s" in sys.argv
 wgs_reset = "-w" in sys.argv
-FRAME_THRESHOLD = 10
+temp_reset = "-t" in sys.argv
+if wgs_reset and temp_reset:
+    raise Exception("Can only use one of -w and -t")
+
+
+FRAME_THRESHOLD = 5
 
 params = json.load(open("PathGenerator/Paths/"+file+".json","r"))
 
@@ -47,7 +52,7 @@ def interpolate(start,end, step_size,shuffle=False):
         idxs = torch.randperm(changes.shape[0])
         changes = changes[idxs,:,:]
 
-    print(changes.shape)
+    # print(changes.shape)
     return changes
 
 
@@ -83,6 +88,7 @@ for i,phase in enumerate(init_phases_ud[0,:]):
 a, b = itertools.tee(positions)
 next(b, None)
 since_reset = 0
+phase_frames = [torch.mean(torch.angle(act_init))]
 for start, end in zip(a, b):
     changes = interpolate(start,end,0.0001,shuffle=shuffle)
     for change in changes:
@@ -92,7 +98,10 @@ for start, end in zip(a, b):
 
         activation_out = net(change) 
         phases = torch.angle(activation_out)
-        pressures.append(torch.abs(propagate(activation_out,point)).detach().numpy())
+        phase_frames.append(torch.mean(phases))
+        
+        p_prop = propagate(activation_out,point)
+        pressures.append(torch.abs(p_prop).detach().numpy())
 
         phases_converted = convert_pats(phases)
 
@@ -111,6 +120,41 @@ for start, end in zip(a, b):
                 A=forward_model(p, transducers()).to(device)
                 _, _, act_init = wgs(A,torch.ones(N,1).to(device)+0j,200)
                 net.init(act_init.T)
+                phase_frames.append(torch.mean(torch.angle(act_init)))
+                pres =torch.abs(propagate(act_init.T,point)).detach()
+                mean_p = torch.mean(pres)
+                pres = pres.numpy()
+                pressures.append(pres)
+                resets[0].append(frames)
+                resets[1].append(mean_p)
+                since_reset = 0
+
+                phases_converted = convert_pats(torch.angle(act_init).T)
+
+                for i,phase in enumerate(phases_converted[0]):
+                    f.write(str(phase.item()))
+                    if i < 511:
+                        f.write(",")
+                    else:
+                        f.write("\n")
+                frames+=1
+        
+        if temp_reset:
+            since_reset+=1
+            if since_reset == FRAME_THRESHOLD:
+                p = torch.squeeze(torch.permute(point,(0,1,2)))
+                A=forward_model(p, transducers()).to(device)
+                
+                T_in=torch.pi/20
+                T_out = 0
+
+                act = activation_out.T
+                prop = torch.unsqueeze(p_prop,1)
+
+                _, _, act_init = temporal_wgs(A,torch.ones(N,1).to(device)+0j,200, act, prop,T_in,T_out)
+                net.init(act_init.T)
+                phase_frames.append(torch.mean(torch.angle(act_init)))
+                
                 pres =torch.abs(propagate(act_init.T,point)).detach()
                 mean_p = torch.mean(pres)
                 pres = pres.numpy()
@@ -142,18 +186,42 @@ f.close()
 
 if "-p" in sys.argv:
     pressures = torch.tensor(pressures)
-   
 
+    phase_frames = [torch.abs(torch.atan2(torch.sin(i),torch.cos(i))) for i in phase_frames]
+    phase_changes = []
+    for i in range(1,len(phase_frames)):
+        phase_changes.append((phase_frames[i] - phase_frames[i-1]).detach().numpy())
+   
+    ax = plt.subplot(2,1,1)
     for i in range(pressures.shape[1]):
-        plt.plot(pressures[:,i],label="point "+str(i))
+        ax.plot(pressures[:,i],label="point "+str(i))
     
-    plt.plot(resets[0],resets[1],"x",label="WGS re-initialisations")
+    ax.plot(resets[0],resets[1],"x",label="Re-initialisations")
     
-    plt.legend()
-    plt.xlabel("Frame")
-    plt.ylabel("Pressure (Pa)")
-    plt.yticks(torch.linspace(0,10000,11).numpy())
-    plt.ylim(bottom=0)
+    ax.legend()
+    ax.set_xlabel("Frame")
+    ax.set_ylabel("Pressure (Pa)")
+    # m = round(torch.min(pressures).item(), -3) - 1000
+    # ax.set_yticks(torch.linspace(m,10000,11).numpy())
+
+    ax.set_yticks(torch.linspace(0,10000,11).numpy())
+
+    ax = plt.subplot(2,1,2)
+    ax.plot(phase_changes)
+    ax.set_ylabel("Phase Change (Rad)")
+    ax.set_xlabel("Frame")
+
+    length = len(phase_changes)
+    phase_changes_reset_frames = [phase_changes[i] for i in resets[0] if i < length]
+    x_resets = [r for r in resets[0] if r < length]
+
+    ax.plot(x_resets,phase_changes_reset_frames,"x",label="Re-initialisations")
+    # if temp_reset:
+    #     ax.plot(torch.linspace(0,frames,frames),torch.ones(frames)*T_in,label="T_in limit = pi/" + str(int(torch.pi/T_in)),color="green")
+    #     ax.plot(torch.linspace(0,frames,frames),torch.ones(frames)*-1*T_in,color="green")
+    ax.legend()
+
+    # plt.ylim(bottom=0)
     plt.show()
 
 ##Animation? https://matplotlib.org/stable/gallery/animation/random_walk.html
